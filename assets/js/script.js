8ae77debf19ce14d44f79e09b69f9f74c7e45d4d
stevey d <stphndcky22@gmail.com>
2022-02-22 12:43:47 -0800

    added expressions to create div element to contain task name and task type inputs, add div element to li element

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,31 @@ var createTaskHandler = function (event) {
     // this will prevent default action of browser refreshing page on submit
     event.preventDefault();
 
+    /* the below expression is selecting for the <input> element with 
+    attribute name = 'task-name'. We add '.value' to the end of our 
+    querySelector to retrieve the value that we input for the object */
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    // this expression does the same except for task-type value under <select>
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    /* these expressions create a div with task name and type, add task name
+    and type to a <li> element, then add that <li> element to a <ul> element */
+    
+    // create li element, give it a class name
     var listItemEl = document.createElement ("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    // create div to hold task info
+    var taskInfoEl = document.createElement("div");
+    // give div a class name
+    taskInfoEl.className = "task-info";
+    // give div header info and task type info
+    // we use .innerHTML because we are adding more html elements
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+    // adds task info div to <li> element
+    listItemEl.appendChild(taskInfoEl);
+    // adds list item to <ul> element
     tasksToDoEl.appendChild(listItemEl);
+
 };
 
 
